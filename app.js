cbccce3529724f2f538346bf6031d36df3367e84
Ian <imacleod@mail.umw.edu>
2022-07-13 16:51:09 -0400

    Add functionality that figures out if there is a winner or not

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 let currentTurn = 0;
 document.querySelector(".playerTurn").textContent = "Current Player's Turn: X"
 
+//Possible cell combinations for winner
+const winningCells = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
+const playerX = [];
+const playerO = [];
+
+function check(playerCells, winningCells) {
+    for (let i = 0; i < winningCells.length; i++) {
+        let count = 0;
+        for (let j = 0; j < playerCells.length; j++) {
+            if (winningCells[i].includes(playerCells[j])) {
+                count++;
+            }
+
+            if (count === 3) {
+                //document.getElementById("result").textContent = "winner:"
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
 document.onclick = (e) => {
     if (currentTurn % 2 == 0) {
         document.querySelector(".playerTurn").textContent = "Current Player's Turn: O"
+        playerX.push(parseInt(e.target.textContent));
+        e.target.classList.add("reveal");
         e.target.textContent = "X";
         currentTurn++;
+        if (check(playerX, winningCells)) {
+            document.getElementById("result").textContent = "winner! Player X"
+        }
     } else {
         document.querySelector(".playerTurn").textContent = "Current Player's Turn: X";
+        playerO.push(parseInt(e.target.textContent));
+        e.target.classList.add("reveal");
         e.target.textContent = "O";
         currentTurn++;
+        if (check(playerO, winningCells)) {
+            document.getElementById("result").textContent = "winner! Player O"
+        }
     }
 }
+
+
 
 
