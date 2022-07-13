8fb6f275a458e1480994103d60401db96554fb1a
Ian <imacleod@mail.umw.edu>
2022-07-13 16:17:06 -0400

    Add text that shows whose turn it is

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-let currentTurn = 1;
+let currentTurn = 0;
+document.querySelector(".playerTurn").textContent = "Current Player's Turn: X"
 
 document.onclick = (e) => {
     if (currentTurn % 2 == 0) {
+        document.querySelector(".playerTurn").textContent = "Current Player's Turn: O"
         e.target.textContent = "X";
         currentTurn++;
     } else {
+        document.querySelector(".playerTurn").textContent = "Current Player's Turn: X";
         e.target.textContent = "O";
         currentTurn++;
     }
 }
 
-
-console.log(`Current turn is ${currentTurn}`);
 
